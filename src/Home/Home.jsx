0eb2df717f0fd3221aaf1d09b704b4d9f06863b6
AlexRicartR <alex.ricart@outlook.com>
2022-12-06 20:27:09 +0100

    Adding home view basic elements, to wit: framework for background image as well as navbar performance

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,23 @@
-const Home = () => <div>THIS IS HOME!</div>;
+// const Home = () => 
+
+// <div>THIS IS HOME!</div>;
+
+// export default Home;
+
+
+import React from "react";
+import background from "../img/home-background-dark-60.jpg";
+// import './Home.css'
+
+function Home() {
+  return (
+    <div style={{ backgroundImage: `url(${background})`,height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat', }}>
+    </div>
+  );
+}
 
 export default Home;
