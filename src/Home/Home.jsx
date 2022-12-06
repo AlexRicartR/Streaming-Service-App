505309ec6bec57cb115660bc502afa82566e2454
AlexRicartR <alex.ricart@outlook.com>
2022-12-06 21:17:16 +0100

    Embedding streaming service logo through prime react

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,8 @@
-// const Home = () => 
-
-// <div>THIS IS HOME!</div>;
-
-// export default Home;
-
-
 import React from "react";
 import background from "../img/home-background-dark-60.jpg";
+import LogoGeeksTv from "../img/geesktv.png";
+import { Image } from 'primereact/image';
+
 // import './Home.css'
 
 function Home() {
@@ -15,7 +11,16 @@ function Home() {
         marginTop:'-70px',
         fontSize:'50px',
         backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat', }}>
+        backgroundRepeat: 'no-repeat', }}>,
+        <Image src="../img/geesktv.png" />
+        <div style={{
+              backgroundImage: `url(${LogoGeeksTv})`, height: '100vh',
+              marginTop: '100px',
+              fontSize: '50px',
+              backgroundRepeat: 'no-repeat',
+              alignContent: 'center'
+          }}>,
+          </div>
     </div>
   );
 }
